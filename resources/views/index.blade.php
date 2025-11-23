3618803167b6e91ef1a30e9da503ba56ab134614
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2025-11-23 05:36:51 +0000

    Setup homepage

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,5 +1,11 @@
 <x-layout>
     <x-slot:title>Home</x-slot:title>
 
-    <h1>Title here</h1>
+    <section class="bg-black bg-gradient text-white text-center" style="margin: -.75rem; padding-top: 6rem; padding-bottom: 6rem;">
+        <div class="container">
+            <h1 class="display-3">DamSecure IoT Portal</h1>
+            <p class="lead mb-4">Connect your project to the Internet with ease</p>
+            <a href="projects" class="btn btn-lg btn-light text-primary fw-bold">Get Started</a>
+        </div>
+    </section>
 </x-layout>
