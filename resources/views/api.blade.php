91f8566cd84e0b5d2782eb4211cb110175c0d0d9
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2025-12-11 08:32:37 +0000

    Automatically generate the right server

--- resources/views/api.blade.php
+++ resources/views/api.blade.php
@@ -9,12 +9,22 @@
 
     <script crossorigin src="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-bundle.js"></script>
     <script>
-        SwaggerUIBundle({
-            url: "../../docs/openapi.yaml",
-            dom_id: "#swagger",
-            defaultModelRendering: "model",
-            defaultModelExpandDepth: 2, // Expand models in route bodies
-            defaultModelsExpandDepth: -1 // Hide models at the bottom of the page
-        });
+        fetch("../../docs/openapi.json")
+            .then(res => res.text())
+            .then(json => {
+                const spec = JSON.parse(json);
+
+                spec.servers = [{
+                    url: "{{ config('app.url') }}/public/api"
+                }]
+
+                SwaggerUIBundle({
+                    dom_id: "#swagger",
+                    defaultModelRendering: "model",
+                    defaultModelExpandDepth: 2, // Expand models in route bodies
+                    defaultModelsExpandDepth: -1, // Hide models at the bottom of the page
+                    spec
+                });
+            })
     </script>
 </x-layout>
