2b63efbf1194cdf515e82dd29fb12cd497da5b52
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2025-12-12 00:03:56 +0000

    Fix paths for production

--- resources/views/api.blade.php
+++ resources/views/api.blade.php
@@ -2,14 +2,14 @@
     <x-slot:title>API Schema</x-slot:title>
     <x-slot:includes>
         <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui.css">
-        <link rel="stylesheet" href="/resources/css/swagger-dark.css">
+        <link rel="stylesheet" href="../resources/css/swagger-dark.css">
     </x-slot:includes>
 
     <div id="swagger"></div>
 
     <script crossorigin src="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-bundle.js"></script>
     <script>
-        fetch("../../docs/openapi.json")
+        fetch("../docs/openapi.json")
             .then(res => res.text())
             .then(json => {
                 const spec = JSON.parse(json);
