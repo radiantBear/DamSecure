1b295ae035a58d9d74f85922164b8ca3913f3452
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2026-02-24 23:10:59 +0000

    Move FAQ to accordion for progressive disclosure

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -15,95 +15,68 @@
         </div>
     </section>
 
-    <div class="row">
-        <div class="col-sm">
-            <h2>What can I upload?</h2>
-            <p>
-                You can upload JSON data, CSV data, or raw text. JSON and CSV data will be
-                parsed and displayed in a table on the website. Each upload is limited to
-                65,535 characters, but you can upload as many times as needed. If you need
-                to upload more than 65 KB at time, you will need to split the upload into
-                several parts. Upload timestamps are automatically tracked and displayed
-                alongside the data.
-            </p>
-        </div>
-        <div class="col-sm">
-            <h2>How do I upload data?</h2>
-            <p>
-                After logging in with your ONID and creating a project, you will be given
-                an an initial API key for uploading data. Uploading is as simple as making
-                an HTTP POST request to the <code>public/api/data</code> endpoint on this
-                site, including a header with the key <code>Authorization</code> and
-                contents matching <code>Bearer {api_key}</code> <i>(where 
-                <code>{api_key}</code>, including the brackets, is replaced with the key
-                you were given)</i> to indicate which project the upload is for. For more
-                details, check out the <a href="docs/api">API schema</a> or
-                <a href="https://github.com/radiantBear/DamSecure/tree/main/docs/examples" target="_blank">
-                code examples</a>.
-            </p>
-        </div>
+    <x-sample-data />
+
+    <h2>FAQ</h2>
+
+    <h3 class="mt-3 mb-2">General</h3>
+    <div class="accordion my-2" id="faqAccordion">
+        <x-accordion-item show accordionId="faqAccordion" id="collapseWhatUpload">
+            <x-slot:header>What can I upload?</x-slot:header>
+
+            You can upload JSON data, CSV data, or raw text. JSON and CSV data will be
+            parsed and displayed in a table on the website. Upload timestamps are
+            automatically tracked and displayed alongside the data.
+        </x-accordion-item>
+        <x-accordion-item accordionId="faqAccordion" id="collapseUploadLimits">
+            <x-slot:header>Are there any limits on uploads?</x-slot:header>
+
+            There is no limit to the total number of uploads you can make. However, each
+            upload is limited to 65,535 characters. If you need to upload more than 65 KB
+            at time, you will need to split the upload into several parts.
+        </x-accordion-item>
+        <x-accordion-item accordionId="faqAccordion" id="collapseRetention">
+            <x-slot:header>How long is my data retained?</x-slot:header>
+
+            All data uploaded to a given project will be preserved for 2 years after the
+            last upload. Once a project has received no uploads for 2 years, it will be
+            automatically deleted.
+        </x-accordion-item>
     </div>
-    <div class="row">
-        <div class="col-sm">
-            <h2>How do I access my data after it's uploaded?</h2>
-            <p>
-                If you just need to upload data and view/share it, simply reopen the
-                project on this site. If you need to manipulate/process the data, it can
-                also be exported as a CSV file (coming soon) or retrieved using the
-                download API key. To share the data with others, you can simply click the
-                project's "permissions" button to invite collaborators or viewers by ONID.
-            </p>
-        </div>
-        <div class="col-sm">
-            <h2>
-                Can I just host my data to download?
-            </h2>
-            <p>
-                Yes! If you just need to test that your project can download and parse
-                your data, you can do this without worrying about manually uploading data!
-                After creating you project in DamSecure, simply use the Test Data section
-                to configure the data that <code>GET public/api/data/test</code> will
-                serve.
-            </p>
-        </div>
-    </div>
-    <div class="row">
-        <div class="col-sm">
-            <h2>How long is my data retained?</h2>
-            <p>
-                All data uploaded to a given project will be preserved for 2 years after
-                the last upload. Once a project has received no uploads for 2 years, it
-                will be auto-deleted.
-            </p>
-        </div>
-    </div>
-    <div class="row">
-        <h2 class="mb-3">What does the web view look like?</h2>
-        <div class="col-sm">
-            <img class="img-fluid" src="img/json_table.png" alt="JSON data parsed into a table">
-            <p>
-                Data uploaded with a <code>Content-Type: application/json</code> header
-                will be parsed as JSON and rejected if it is improperly formatted. When
-                viewed, the top-level fields will be parsed to form table columns.
-            </p>
-        </div>
-        <div class="col-sm">
-            <img class="img-fluid" src="img/csv_table.png" alt="CSV data parsed into a table">
-            <p>
-                Data uploaded with a <code>Content-Type: text/csv</code> header
-                will be parsed as CSV data using the rules of PHP's
-                <code>str_getcsv()</code> function. When viewed, the fields will be parsed
-                to form table columns. Since CSV fields are unnamed, columns will always
-                be filled left-to-right with corresponding fields' data.
-            </p>
-        </div>
-        <div class="col-sm">
-            <img class="img-fluid" src="img/plain_table.png" alt="Raw data parsed into a table">
-            <p>
-                Data uploaded with a <code>Content-Type: text/plain</code> header (or no
-                <code>Content-Type</code> header) will be left untouched. Upload
-                timestamps will still be displayed along with the data.
-            </p>
-        </div>
+    
+    <h3 class="mt-3 mb-2">Usage</h3>
+    <div class="accordion" id="usageAccordion">
+        <x-accordion-item accordionId="usageAccordion" id="collapseHowUpload">
+            <x-slot:header>How do I upload data?</x-slot:header>
+
+            After logging in with your ONID and creating a project, you will be given an
+            initial API key for uploading data. Uploading is as simple as making an HTTP
+            POST request to the <code>public/api/data</code> endpoint on this site,
+            including a header with the key <code>Authorization</code> and contents
+            matching <code>Bearer {api_key}</code> <i>(where <code>{api_key}</code>,
+            including the brackets, is replaced with the key you were given)</i> to
+            indicate which project the upload is for. For more details, check out the
+            <a href="docs/api">API schema</a> or
+            <a href="https://github.com/radiantBear/DamSecure/tree/main/docs/examples" target="_blank">
+                code examples
+            </a>.
+        </x-accordion-item>
+        <x-accordion-item accordionId="usageAccordion" id="collapseAccessUpload">
+            <x-slot:header>How do I access my data after it's uploaded?</x-slot:header>
+
+            If you just need to upload data and view/share it, simply reopen the project
+            on this site. If you need to manipulate/process the data, it can also be
+            exported as a CSV file (coming soon) or retrieved using the download API key.
+            To share the data with others, you can simply click the project's
+            "permissions" button to invite collaborators or viewers by ONID.
+        </x-accordion-item>
+        <x-accordion-item accordionId="usageAccordion" id="collapseHostDownload">
+            <x-slot:header>Can I just host my data to download?</x-slot:header>
+
+            Yes! If you just need to test that your project can download and parse your
+            data, you can do this without worrying about manually uploading data. After
+            creating you project in DamSecure, simply use the Test Data section to
+            configure the data that <code>GET public/api/data/test</code> will serve.
+        </x-accordion-item>
     </div>
 </x-layout>
