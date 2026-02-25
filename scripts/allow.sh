0228531cb50eabc590693063689accbaf039160a
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2026-02-25 00:59:46 +0000

    Fix permissions script

--- scripts/allow.sh
+++ scripts/allow.sh
@@ -2,7 +2,7 @@
 #
 # On the OSU servers, files and directories have to have certain permissions in order to
 # be accessible from a browser. We set the correct permissions here.
-for f in $(find . \( -name "*.php" -o -path "./resources/*" -o -path "./public/*" \) -not -path "./vendor/*" -not -path "./storage/*"); do
+for f in $(find . \( -name "*.php" -o -path "./resources/*" -o -path "./public/*" -o -path "./docs/openapi.json" \) -not -path "./vendor/*" -not -path "./storage/*"); do
     if [ "$f" = "." ] || [ "$f" = ".." ]; then
         continue
     fi
