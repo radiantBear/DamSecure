178449d6486332de879672f637e35afd75094998
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2026-03-03 01:46:06 +0000

    Update swagger-ui

--- resources/views/api.blade.php
+++ resources/views/api.blade.php
@@ -1,13 +1,13 @@
 <x-layout>
     <x-slot:title>API Schema</x-slot:title>
     <x-slot:includes>
-        <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui.css">
+        <link rel="stylesheet" type="text/css" href="https://unpkg.com/swagger-ui-dist@5.31.2/swagger-ui.css">
         <link rel="stylesheet" href="../resources/css/swagger-dark.css">
     </x-slot:includes>
 
     <div id="swagger"></div>
 
-    <script crossorigin src="https://unpkg.com/swagger-ui-dist@5.11.0/swagger-ui-bundle.js"></script>
+    <script crossorigin src="https://unpkg.com/swagger-ui-dist@5.31.2/swagger-ui-bundle.js"></script>
     <script>
         fetch("../docs/openapi.json")
             .then(res => res.text())
