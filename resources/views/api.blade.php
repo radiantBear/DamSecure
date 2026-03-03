3f7667c976a9d893832eb08089898552ef372e15
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2026-03-03 01:47:23 +0000

    Generate Python examples

--- resources/views/api.blade.php
+++ resources/views/api.blade.php
@@ -9,6 +9,56 @@
 
     <script crossorigin src="https://unpkg.com/swagger-ui-dist@5.31.2/swagger-ui-bundle.js"></script>
     <script>
+        // Define Request Snippet for displaying Python code example
+        // https://swagger.io/docs/open-source-tools/swagger-ui/customization/plug-points/#request-snippets
+        const SnippedGeneratorPythonPlugin = {
+            fn: {
+                requestSnippetGenerator_python: (request) => {
+                    const headers = request.get("headers");
+                    let reqBody = request.get("body")
+                    if (reqBody)
+                        reqBody = JSON.stringify(reqBody)
+
+                    const showBody = reqBody || (
+                        request.get("method") !== "GET" && request.get("method") !== "DELETE"
+                    );
+                    console.log(reqBody)
+
+                    const stringBody = "(\n" +
+                        (
+                            (reqBody || "")
+                            .replace(/\\n/g, "\n")
+                            .replace(/\n/g, "\"\n")
+                            .substring(1)
+                            .split("\n")
+                            .map(l => `    "${l}`)
+                            .join("\n")
+                        )
+                        + "\n  ).encode(\"utf-8\"),";
+                    console.log(stringBody)
+
+                    return (
+`from urllib.request import urlopen, Request
+
+request = Request(
+  url="${request.get("url")}"${showBody ? `,
+  data=${reqBody ? stringBody : "\"\""}` : "," }
+  method="${request.get("method").toUpperCase()}"${headers && headers.size ? `,
+  headers={
+    ${request.get("headers").map((val, key) => `"${key}": "${val}"`).valueSeq().join(",\n    ")}
+  }` : ""}
+)
+
+with urlopen(request) as response:
+  print(f"Status: {response.status}")
+  
+  # If uploading succeeds, this gives the ID for updating/deleting the upload
+  print(f"Response: {response.read().decode("utf-8")}")`
+                    );
+                }
+            }
+        }
+
         fetch("../docs/openapi.json")
             .then(res => res.text())
             .then(json => {
@@ -23,15 +73,19 @@
                     defaultModelRendering: "model",
                     defaultModelExpandDepth: 2, // Expand models in route bodies
                     defaultModelsExpandDepth: -1, // Hide models at the bottom of the page
+                    plugins: [
+                        SnippedGeneratorPythonPlugin
+                    ],
                     requestSnippetsEnabled: true,
                     requestSnippets: {
                         generators: {
                             curl_bash: { title: "cURL (bash)", syntax: "bash" },
                             curl_powershell: { title: "cURL (PowerShell)", syntax: "powershell" },
                             curl_cmd: {},
+                            python: { title: "Python", syntax: "python" },
                         },
                         defaultExpanded: true,
-                        languages: ['curl_bash', 'curl_powershell']
+                        languages: ['python', 'curl_bash', 'curl_powershell']
                     },
                     spec
                 });
