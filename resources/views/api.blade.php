44e8a6387a149805493ddfd902f2da74d7acd822
Nate Baird <85893453+radiantBear@users.noreply.github.com>
2026-05-31 08:12:25 +0000

    Add generator for example Arduino code to API schema viewer

--- resources/views/api.blade.php
+++ resources/views/api.blade.php
@@ -22,7 +22,6 @@
                     const showBody = reqBody || (
                         request.get("method") !== "GET" && request.get("method") !== "DELETE"
                     );
-                    console.log(reqBody)
 
                     const stringBody = "(\n" +
                         (
@@ -35,7 +34,6 @@
                             .join("\n")
                         )
                         + "\n  ).encode(\"utf-8\"),";
-                    console.log(stringBody)
 
                     return (
 `from urllib.request import urlopen, Request
@@ -58,6 +56,115 @@ with urlopen(request) as response:
                 }
             }
         }
+        
+        // Define Request Snippet for displaying Arduino code example
+        // https://swagger.io/docs/open-source-tools/swagger-ui/customization/plug-points/#request-snippets
+        const SnippedGeneratorArduinoPlugin = {
+            fn: {
+                requestSnippetGenerator_arduino: (request) => {
+                    const hostname = new URL(request.get("url")).hostname
+                    const headers = request.get("headers");
+                    const api_token = request.get('headers').get('Authorization');
+
+                    let reqBody = request.get("body")
+                    if (reqBody)
+                        reqBody = JSON.stringify(reqBody)
+
+                    const showBody = reqBody || (
+                        request.get("method") !== "GET" && request.get("method") !== "DELETE"
+                    );
+
+                    const stringBody = ((reqBody || "")
+                        .replace(/\\n/g, "\n")
+                        .replace(/\n/g, "\"\n")
+                        .substring(1)
+                        .split("\n")
+                        .map((l, i) => i > 0 ? `                           "${l}` : l)
+                        .join("\n")
+                        .slice(0, -1)
+                        );
+
+                    return (
+`#include <HTTPClient.h>
+#include <NetworkClientSecure.h>
+
+const char *rootCACertificate = ""; // TODO: Get root CA certificate for ${hostname}
+const char *damsecure_url = "${request.get("url")}";
+const char *api_token = "${api_token ? api_token + '";' : '"; // TODO: Get API token'}${showBody ? `
+const char *request_body = "${stringBody}";` : '' }
+
+void makeRequest() {
+  NetworkClientSecure *client = new NetworkClientSecure;
+
+  if (!client) {
+    Serial.println("Unable to allocate client");
+    return;
+  }
+
+  client->setCACert(rootCACertificate);
+  _sendRequest(client, data);
+  delete client;
+}
+
+void _sendRequest(NetworkClientSecure *client) {
+  // Using a different scoping block (function) for HTTPClient https to make sure it is
+  // destroyed before NetworkClientSecure *client is
+  HTTPClient https;
+
+  Serial.println("Connecting to DamSecure...");
+  if (!https.begin(*client, damsecure_url)) {
+    Serial.println("Unable to connect to DamSecure");
+    return;
+  }
+
+  Serial.println("Making ${request.get('method')} request...");
+  https.addHeader("Authorization", api_token);
+  ${
+    request.get("headers")
+      .map((val, key) => `https.addHeader("${key}", "${val}");`)
+      .valueSeq()
+      .filter(val => !val.startsWith('https.addHeader("Authorization",'))
+      .join("\n  ")}
+  int httpCode = https.${request.get('method')}(${showBody ? 'request_body' : ''});
+
+  processResponse(&https, httpCode);
+  https.end();
+}
+
+void processResponse(HTTPClient *https, int httpCode) {
+  // httpCode will be negative on error
+  if (httpCode <= 0) {
+    Serial.print("${request.get('method')} failed, error: ");
+    Serial.println(https->errorToString(httpCode).c_str());
+    return;
+  }
+    
+  // HTTP request has been sent and server response header has been handled
+  Serial.printf("${request.get('method')} response code: %d\\n", httpCode);
+
+  // server accepted request
+  if (
+    httpCode == HTTP_CODE_OK ||
+    httpCode == HTTP_CODE_CREATED ||
+    httpCode == HTTP_CODE_MOVED_PERMANENTLY
+  ) {
+    String payload = https->getString();
+    Serial.print("${request.get('method')} accepted; response: ");
+    Serial.println(payload);
+  }
+}
+
+void main() {
+  Serial.begin(115200);
+
+  // TODO: Connect to Internet (WiFi, Ethernet, etc.)
+
+  makeRequest();
+}`
+                    );
+                }
+            }
+        }
 
         fetch("../docs/openapi.json")
             .then(res => res.text())
@@ -74,7 +181,8 @@ with urlopen(request) as response:
                     defaultModelExpandDepth: 2, // Expand models in route bodies
                     defaultModelsExpandDepth: -1, // Hide models at the bottom of the page
                     plugins: [
-                        SnippedGeneratorPythonPlugin
+                        SnippedGeneratorPythonPlugin,
+                        SnippedGeneratorArduinoPlugin
                     ],
                     requestSnippetsEnabled: true,
                     requestSnippets: {
@@ -83,9 +191,10 @@ with urlopen(request) as response:
                             curl_powershell: { title: "cURL (PowerShell)", syntax: "powershell" },
                             curl_cmd: {},
                             python: { title: "Python", syntax: "python" },
+                            arduino: { title: "ESP-32 (Arduino)", syntax: "c" }
                         },
                         defaultExpanded: true,
-                        languages: ['python', 'curl_bash', 'curl_powershell']
+                        languages: ['python', "arduino", 'curl_bash', 'curl_powershell']
                     },
                     spec
                 });
